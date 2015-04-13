d6fc3fed63d254e564e971bd61a65666e95e28c4
Paulo Pereira <paulojp@student.dei.uc.pt>
2015-04-13 21:19:22 +0100

    added web service files

--- Assignment_1/Projecto/src/client/ObjectFactory.java
+++ Assignment_1/Projecto/src/client/ObjectFactory.java
@@ -24,8 +24,12 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _ForecastResponse_QNAME = new QName("http://server/", "forecastResponse");
-    private final static QName _Forecast_QNAME = new QName("http://server/", "forecast");
+    private final static QName _PersonalSensitivityToInsulinResponse_QNAME = new QName("http://server/", "personalSensitivityToInsulinResponse");
+    private final static QName _MealtimeInsulinDoseResponse_QNAME = new QName("http://server/", "mealtimeInsulinDoseResponse");
+    private final static QName _BackgroundInsulinDoseResponse_QNAME = new QName("http://server/", "backgroundInsulinDoseResponse");
+    private final static QName _MealtimeInsulinDose_QNAME = new QName("http://server/", "mealtimeInsulinDose");
+    private final static QName _BackgroundInsulinDose_QNAME = new QName("http://server/", "backgroundInsulinDose");
+    private final static QName _PersonalSensitivityToInsulin_QNAME = new QName("http://server/", "personalSensitivityToInsulin");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: client
@@ -35,37 +39,105 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link Forecast }
+     * Create an instance of {@link PersonalSensitivityToInsulin }
      * 
      */
-    public Forecast createForecast() {
-        return new Forecast();
+    public PersonalSensitivityToInsulin createPersonalSensitivityToInsulin() {
+        return new PersonalSensitivityToInsulin();
     }
 
     /**
-     * Create an instance of {@link ForecastResponse }
+     * Create an instance of {@link BackgroundInsulinDose }
      * 
      */
-    public ForecastResponse createForecastResponse() {
-        return new ForecastResponse();
+    public BackgroundInsulinDose createBackgroundInsulinDose() {
+        return new BackgroundInsulinDose();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link ForecastResponse }{@code >}}
+     * Create an instance of {@link PersonalSensitivityToInsulinResponse }
      * 
      */
-    @XmlElementDecl(namespace = "http://server/", name = "forecastResponse")
-    public JAXBElement<ForecastResponse> createForecastResponse(ForecastResponse value) {
-        return new JAXBElement<ForecastResponse>(_ForecastResponse_QNAME, ForecastResponse.class, null, value);
+    public PersonalSensitivityToInsulinResponse createPersonalSensitivityToInsulinResponse() {
+        return new PersonalSensitivityToInsulinResponse();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link Forecast }{@code >}}
+     * Create an instance of {@link MealtimeInsulinDoseResponse }
      * 
      */
-    @XmlElementDecl(namespace = "http://server/", name = "forecast")
-    public JAXBElement<Forecast> createForecast(Forecast value) {
-        return new JAXBElement<Forecast>(_Forecast_QNAME, Forecast.class, null, value);
+    public MealtimeInsulinDoseResponse createMealtimeInsulinDoseResponse() {
+        return new MealtimeInsulinDoseResponse();
+    }
+
+    /**
+     * Create an instance of {@link MealtimeInsulinDose }
+     * 
+     */
+    public MealtimeInsulinDose createMealtimeInsulinDose() {
+        return new MealtimeInsulinDose();
+    }
+
+    /**
+     * Create an instance of {@link BackgroundInsulinDoseResponse }
+     * 
+     */
+    public BackgroundInsulinDoseResponse createBackgroundInsulinDoseResponse() {
+        return new BackgroundInsulinDoseResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link PersonalSensitivityToInsulinResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://server/", name = "personalSensitivityToInsulinResponse")
+    public JAXBElement<PersonalSensitivityToInsulinResponse> createPersonalSensitivityToInsulinResponse(PersonalSensitivityToInsulinResponse value) {
+        return new JAXBElement<PersonalSensitivityToInsulinResponse>(_PersonalSensitivityToInsulinResponse_QNAME, PersonalSensitivityToInsulinResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MealtimeInsulinDoseResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://server/", name = "mealtimeInsulinDoseResponse")
+    public JAXBElement<MealtimeInsulinDoseResponse> createMealtimeInsulinDoseResponse(MealtimeInsulinDoseResponse value) {
+        return new JAXBElement<MealtimeInsulinDoseResponse>(_MealtimeInsulinDoseResponse_QNAME, MealtimeInsulinDoseResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link BackgroundInsulinDoseResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://server/", name = "backgroundInsulinDoseResponse")
+    public JAXBElement<BackgroundInsulinDoseResponse> createBackgroundInsulinDoseResponse(BackgroundInsulinDoseResponse value) {
+        return new JAXBElement<BackgroundInsulinDoseResponse>(_BackgroundInsulinDoseResponse_QNAME, BackgroundInsulinDoseResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MealtimeInsulinDose }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://server/", name = "mealtimeInsulinDose")
+    public JAXBElement<MealtimeInsulinDose> createMealtimeInsulinDose(MealtimeInsulinDose value) {
+        return new JAXBElement<MealtimeInsulinDose>(_MealtimeInsulinDose_QNAME, MealtimeInsulinDose.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link BackgroundInsulinDose }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://server/", name = "backgroundInsulinDose")
+    public JAXBElement<BackgroundInsulinDose> createBackgroundInsulinDose(BackgroundInsulinDose value) {
+        return new JAXBElement<BackgroundInsulinDose>(_BackgroundInsulinDose_QNAME, BackgroundInsulinDose.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link PersonalSensitivityToInsulin }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://server/", name = "personalSensitivityToInsulin")
+    public JAXBElement<PersonalSensitivityToInsulin> createPersonalSensitivityToInsulin(PersonalSensitivityToInsulin value) {
+        return new JAXBElement<PersonalSensitivityToInsulin>(_PersonalSensitivityToInsulin_QNAME, PersonalSensitivityToInsulin.class, null, value);
     }
 
 }
